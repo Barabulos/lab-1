88322662c2341dc34fd3c4d62e38f81b276cdc0c
Barabulos <vidreanrobert@yahoo.com>
2023-10-29 19:47:49 +0200

    Test zahlen gata

--- src/ZahlenTest.java
+++ src/ZahlenTest.java
@@ -5,7 +5,6 @@ public class ZahlenTest {
         int[] array2 = {};
         int[] array3 = {5};
 
-        // Teste maximaleZahl
         assert zahlen.maximaleZahl(array1) == 17;
         assert zahlen.maximaleZahl(array2) == -1;
         assert zahlen.maximaleZahl(array3) == 5;
@@ -15,7 +14,6 @@ public class ZahlenTest {
         int[] array1 = {4, 8, 3, 10, 17};
         int[] array2 = {};
         int[] array3 = {5};
-        // Teste minimaleZahl
         assert zahlen.minimaleZahl(array1) == 3 ;
         assert zahlen.minimaleZahl(array2) == -1 ;
         assert zahlen.minimaleZahl(array3) == 5 ;
@@ -25,7 +23,7 @@ public class ZahlenTest {
         int[] array1 = {4, 8, 3, 10, 17};
         int[] array2 = {};
         int[] array3 = {5};
-        // Teste summe
+
         assert zahlen.summe(array1) == 42;
         assert zahlen.summe(array2) == -1;
         assert zahlen.summe(array3) == 5;
